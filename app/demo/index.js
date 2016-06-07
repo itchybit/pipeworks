0cf007478a3928af962dec67c3675d7b6786091d
Toni <pesola.toni@gmail.com>
2016-06-07 22:21:24 +0300

    Added jsRocket. Modified demo index to use jsRocket

--- app/demo/index.js
+++ app/demo/index.js
@@ -1,9 +1,45 @@
+import JSRocket from './services/jsRocket';
 
+const VALUES = new Map()
 
 class Demo {
-  init() {
-    console.log("Demo started");
+
+  constructor() {
+    this.syncDevice = new JSRocket.SyncDevice();
+    this.row = 0;
   }
+
+  init() {
+    console.log("Demo init");
+    this.syncDevice.init();
+
+    this.syncDevice.on('ready', () => {this.onSyncReady()});
+    this.syncDevice.on('update', (row) => {this.onSyncUpdate(row)});
+    this.syncDevice.on('play', () => {this.onPlay()});
+    this.syncDevice.on('pause', () => {this.onPause()});
+  }
+
+  updateValues() {
+
+  }
+
+  onSyncReady() {
+    console.log("onSyncReady");
+  }
+
+  onSyncUpdate(row) {
+    console.log("onSyncUpdate", row);
+    this.row = row;
+  }
+
+  onPlay() {
+    console.log("onPlay");
+  }
+
+  onPause() {
+    console.log("onPause");
+  }
+
 }
 
 export default new Demo();
